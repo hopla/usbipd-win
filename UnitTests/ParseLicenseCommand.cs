1b0aa33122f0d4f4201b6558e450c86460222520
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2022-01-04 09:00:11 +0100

    Initial attempt of moving to System.CommandLine

---
+++ UnitTests/ParseLicenseCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.CommandLine;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using UsbIpServer;
+
+namespace UnitTests
+{
+    using ExitCode = Program.ExitCode;
+
+    [TestClass]
+    public class ParseLicenseCommand
+        : ParseTest
+    {
+        [TestMethod]
+        public void Success()
+        {
+            var mock = CreateMock();
+            mock.Setup(m => m.License(
+                It.IsNotNull<IConsole>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(true));
+
+            Test(ExitCode.Success, mock, "license");
+        }
+
+        [TestMethod]
+        public void Failed()
+        {
+            var mock = CreateMock();
+            mock.Setup(m => m.License(
+                It.IsNotNull<IConsole>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(false));
+
+            Test(ExitCode.Failure, mock, "license");
+        }
+
+        [TestMethod]
+        public void Help()
+        {
+            Test(ExitCode.Success, "license", "--help");
+        }
+
+        [TestMethod]
+        public void StrayArgument()
+        {
+            Test(ExitCode.ParseError, "license", "stray-argument");
+        }
+    }
+}
